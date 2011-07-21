1653f5e9e1dcf4cfd59dd58c7fcc69d81d4c7528
tratatun <tratatun4k@ukr.net>
2011-07-21 15:48:36 +0300

    fix request handlers11

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,18 +2,19 @@ var exec = require("child_process").exec;
 
 function start(res) {
     console.log("Request handler 'start' was called." + new Date().toString() +"\nres:");
-    console.dir(exec);
-    exec("ls",
+    //console.dir(exec);
+    exec("find /",
+        { timeout: 10000, maxBuffer: 20000*1024 },
         function (error, stdout, stderr) {
-        console.dir(process.res);
-        process.res.writeHead(200, {"Content-Type":"text/html"});
-        process.res.write(stdout);
-        process.res.write("\n wha" + stdout.toString() + "tthf?!");
-        console.dir(error);
-        console.dir(stdout);
-        console.dir(stderr);
-        process.res.end();
-    });
+            console.dir(res);
+            res.writeHead(200, {"Content-Type":"text/html"});
+            //process.res.write(stdout);
+            res.write("\n whatthf?!");
+            console.dir(error);
+            console.dir(stdout);
+            console.dir(stderr);
+            res.end();
+        });
 }
 
 function upload(res) {
