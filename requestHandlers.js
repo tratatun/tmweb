6794cc7854fe37bb890b610771c9b6ca9b33a773
tratatun <tratatun4k@ukr.net>
2011-07-21 15:54:59 +0300

    fix request handlers12

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,24 +1,30 @@
 var exec = require("child_process").exec;
 
 function start(res) {
-    console.log("Request handler 'start' was called." + new Date().toString() +"\nres:");
+    console.log("'start' was called." + new Date().toString() +"\nres:");
     //console.dir(exec);
     exec("find /",
         { timeout: 10000, maxBuffer: 20000*1024 },
         function (error, stdout, stderr) {
-            console.dir(res);
-            res.writeHead(200, {"Content-Type":"text/html"});
-            //process.res.write(stdout);
-            res.write("\n whatthf?!");
-            console.dir(error);
-            console.dir(stdout);
-            console.dir(stderr);
-            res.end();
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(stdout);
+            response.end();
         });
+    /*
+        function (error, stdout, stderr) {
+            //console.dir(res);
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.write(stdout);
+            //res.write("\n whatthf?!");
+            //console.dir(error);
+            //console.dir(stdout);
+            //console.dir(stderr);
+            res.end();
+        });*/
 }
 
 function upload(res) {
-    console.log("Request handler 'upload' was called." + new Date().toString())
+    console.log("'upload' was called." + new Date().toString())
     res.writeHead(200, {"Content-Type":"text/html"});
     res.write("Hello Upload, !!!!!");
     res.end();
