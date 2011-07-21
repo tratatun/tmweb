6db1aac37261391dec2802249dc86b9e6f6c0c83
tratatun <tratatun4k@ukr.net>
2011-07-21 18:24:34 +0300

    request handler with html

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,13 +2,31 @@ var exec = require("child_process").exec;
 
 function start(response) {
     console.log("'start' was called." + new Date().toString() +"\nres:");
+    
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">тратата'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+
+    /*
     //console.dir(exec);
-    exec("ls -lah",
+    exec("dir",
         { timeout: 10000, maxBuffer: 20000*1024 },
         function (error, stdout, stderr) {
-            console.dir(error);
+            //console.dir(error);
             console.dir(stdout);
-            console.dir(stderr);
+            //console.dir(stderr);
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(stdout);
             response.end();
@@ -23,7 +41,8 @@ function start(response) {
             //console.dir(stdout);
             //console.dir(stderr);
             res.end();
-        });*/
+        });
+        */
 }
 
 function upload(res) {
